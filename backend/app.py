d3b642f17a64e505e26636f1092343e17a27c2dd
marcus159260 <marcus.yeo.2022@scis.smu.edu.sg>
2024-09-14 15:31:54 +0800

    committing changes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,18 @@
 from flask import Flask
 from routes.user_routes import user_bp
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Access the Supabase URL and keys
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+# Now you can use these variables in your application
+print(f"Supabase URL: {supabase_url}")
+print(f"Supabase Key: {supabase_key}")
 
 app = Flask(__name__)
 
