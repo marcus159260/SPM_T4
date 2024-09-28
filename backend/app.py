1c6929f5ba5915aaa878220fa9ce91c909bb1f10
Jun Hao <77221369+Ecksdee03@users.noreply.github.com>
2024-09-28 21:49:23 +0800

    backend get employee data from supabase

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.user_routes import user_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,resources={r"/*":{'origins':"*"}})
 
-app.register_blueprint(user_bp, url_prefix='/api/users/')
+app.register_blueprint(user_bp, url_prefix="/users")
 
 @app.route("/")
 def index(): 
