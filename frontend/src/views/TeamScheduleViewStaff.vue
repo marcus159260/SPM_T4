7b1fa48e7b043517db9fda7853cbda34b4c61347
Jermaine Chan <147256123+jermaineclx@users.noreply.github.com>
2024-11-04 17:20:25 +0800

    Update TeamScheduleViewStaff.vue can +-7 days

--- frontend/src/views/TeamScheduleViewStaff.vue
+++ frontend/src/views/TeamScheduleViewStaff.vue
@@ -3,14 +3,14 @@
       <teamCalendar
         :title="'My Team Schedule'"
         :resources="resources"
+        @dateChanged="onDateChanged"
+
       />
     </div>
   </template>
   
   <script>
   import teamCalendar from '../components/StaffScheduleView/teamCalendar.vue';
-  import axios from 'axios';
-  // import { getTeamEvents, getTeamResources } from '../services/apiService';
   
   export default {
     components: {
@@ -22,22 +22,9 @@
         teamResources: [],
         employees:[],
         resources:[],
-        Staff_ID: 140894 //hardcode team manager
         
       };
-    },
-    mounted() {
-      // var url = "http://127.0.0.1:5000/api/users/by-team-employees/" + config.reporting_manager;
-      // axios
-      //   .get(url)
-      //   .then((response) => {
-      //     this.resources = response.data;
-      //     console.log("Loaded resources:", this.resources);
-      //   })
-      //   .catch((error) => {
-      //     console.error('Error fetching requests:', error);
-      //   });
-    },
+    }
     
   };
   
