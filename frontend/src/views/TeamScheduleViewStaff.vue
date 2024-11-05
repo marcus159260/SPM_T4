78213a48857196c88e4d2473bf82fe1e4c88394e
Jermaine Chan <147256123+jermaineclx@users.noreply.github.com>
2024-11-05 23:11:01 +0800

    Update TeamScheduleViewStaff.vue

--- frontend/src/views/TeamScheduleViewStaff.vue
+++ frontend/src/views/TeamScheduleViewStaff.vue
@@ -11,6 +11,7 @@
   
   <script>
   import teamCalendar from '../components/StaffScheduleView/teamCalendar.vue';
+  import { useAuthStore } from '@/stores/auth';
   
   export default {
     components: {
@@ -22,6 +23,8 @@
         teamResources: [],
         employees:[],
         resources:[],
+        Staff_ID: useAuthStore().user.staff_id
+
         
       };
     }
