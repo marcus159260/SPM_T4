a32297d47ffd340f291167ce7904aa9ebd811a31
marcus159260 <marcus.yeo.2022@scis.smu.edu.sg>
2024-11-06 03:05:56 +0800

    minor UI changes

--- frontend/src/components/ManagerScheduleView/RejectedRequests.vue
+++ frontend/src/components/ManagerScheduleView/RejectedRequests.vue
@@ -1,51 +1,60 @@
 <template>
     <div>
-        <h6 id="pending-header" v-if="managerDetails" class="mt-10">
-            Manager Name: <span>{{ managerDetails.Full_Name }}</span> <br />
-            Manager ID: <span>{{ managerDetails.Staff_ID }}</span> <br>
-            Department: <span>{{ managerDetails.Department }}</span> <br />
-            Position: <span>{{ managerDetails.Position }}</span> <br />
-            <!-- In charge of: <br>
+        <div class="row">
+            <div class="col-12">
+                <h6 id="pending-header" v-if="managerDetails" class="mt-10">
+                    Manager Name: <span>{{ managerDetails.Full_Name }}</span> <br />
+                    Manager ID: <span>{{ managerDetails.Staff_ID }}</span> <br>
+                    Department: <span>{{ managerDetails.Department }}</span> <br />
+                    Position: <span>{{ managerDetails.Position }}</span> <br />
+                    <!-- In charge of: <br>
           &emsp;Dept -> <span>{{ managerDetails.Department }}</span> <br>
           &emsp;Position -> <span>{{ managerDetails.Position }}</span> -->
-        </h6>
+                </h6>
+            </div>
+        </div>
+
         <button @click="fetchRequests" class="btn btn-primary mt-3">Refresh Requests</button>
-        <table v-if="rejectedRequests.length > 0" class="table align-middle mt-10 bg-white">
-            <thead class="bg-light">
-                <tr>
-                    <th scope="col">Request ID</th>
-                    <th scope="col">Staff Name</th>
-                    <th scope="col">Department</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Status</th>
-                    <th scope="col" class="date-column">Request Date</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Request Type</th>
-                    <th scope="col">Request Reason</th>
-                    <th scope="col" class="date-column">Application Date</th>
-                    <th scope="col" class="date-column">WFH Start Date</th>
-                    <th scope="col">Reason of Application</th>
-                    <th scope="col">Withdrawal Reason</th>
-                </tr>
-            </thead>
-            <tbody v-for="staff in rejectedRequests" :key="staff.Staff_ID">
-                <tr>
-                    <th scope="row">{{ staff.Request_ID }}</th>
-                    <td>{{ staff.Staff_Name }}</td>
-                    <td>{{ staff.Staff_Department }}</td>
-                    <td>{{ staff.Staff_Position }}</td>
-                    <td>{{ staff.Status }}</td>
-                    <td>{{ formatDate(staff.Start_Date) }}</td>
-                    <td>{{ staff.Time }}</td>
-                    <td>{{ staff.Request_Type }}</td>
-                    <td>{{ staff.Reason }}</td>
-                    <td>{{ formatDate(staff.Application_Date) }}</td>
-                    <td>{{ formatDate(staff.Start_Date) }}</td>
-                    <td>{{ staff.Rejection_Reason }}</td>
-                    <td>{{ staff.Withdrawal_Reason }}</td>
-                </tr>
-            </tbody>
-        </table>
+
+        <div class="table-responsive">
+            <table v-if="rejectedRequests.length > 0" class="table align-middle mt-10 bg-white">
+                <thead class="bg-light">
+                    <tr>
+                        <th scope="col">Request ID</th>
+                        <th scope="col">Staff Name</th>
+                        <th scope="col">Department</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">Status</th>
+                        <th scope="col" class="date-column">Request Date</th>
+                        <th scope="col">Time</th>
+                        <th scope="col">Request Type</th>
+                        <th scope="col">Request Reason</th>
+                        <th scope="col" class="date-column">Application Date</th>
+                        <th scope="col" class="date-column">WFH Start Date</th>
+                        <th scope="col">Reason of Application</th>
+                        <th scope="col">Withdrawal Reason</th>
+                    </tr>
+                </thead>
+                <tbody v-for="staff in rejectedRequests" :key="staff.Staff_ID">
+                    <tr>
+                        <th scope="row">{{ staff.Request_ID }}</th>
+                        <td>{{ staff.Staff_Name }}</td>
+                        <td>{{ staff.Staff_Department }}</td>
+                        <td>{{ staff.Staff_Position }}</td>
+                        <td>{{ staff.Status }}</td>
+                        <td>{{ formatDate(staff.Start_Date) }}</td>
+                        <td>{{ staff.Time }}</td>
+                        <td>{{ staff.Request_Type }}</td>
+                        <td>{{ staff.Reason }}</td>
+                        <td>{{ formatDate(staff.Application_Date) }}</td>
+                        <td>{{ formatDate(staff.Start_Date) }}</td>
+                        <td>{{ staff.Rejection_Reason }}</td>
+                        <td>{{ staff.Withdrawal_Reason }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
         <div v-if="rejectedRequests.length === 0" class="text-center mt-3">
             <p>No Rejected requests.</p>
         </div>
@@ -89,7 +98,7 @@ export default {
 
                 .then(response => {
                     this.allRequests = response.data;
-                    console.log(this.allRequests)
+                    // console.log(this.allRequests)
                 })
                 .catch(error => {
                     console.error('Error fetching requests:', error);
@@ -103,9 +112,9 @@ export default {
         },
         rejectedRequests() {
             // Filter for approved requests
-            return this.allRequests.filter(request => 
-            request.Status === 'Rejected' && 
-            request.Approver_ID === this.managerId
+            return this.allRequests.filter(request =>
+                request.Status === 'Rejected' &&
+                request.Approver_ID === this.managerId
             );
         }
     },
@@ -126,6 +135,7 @@ export default {
 }
 
 .date-column {
-    min-width: 120px; /* Adjust width as needed */
+    min-width: 120px;
+    /* Adjust width as needed */
 }
 </style>
